ea83617eecbfdb82c25c9a76e5ad3e75ae10d176
badtiming <graysonliv@gmail.com>
2022-08-11 21:33:54 -0400

    linked script.js to index.html, date display added and functioning properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //variable for appending class pastpresentfuture
 //for loop to take the current time and apply past, present or future
-var pastpresentfuture = document.getElementById("#ppf")
-
+var pastpresentfuture = document.getElementById("#ppf");
+var timeDisplay =  $("#currentDay");
 // if (condition) {
     
 // } else if (condition) {
@@ -9,3 +9,11 @@ var pastpresentfuture = document.getElementById("#ppf")
 // } else {
     
 // }
+
+
+function displayTime() {
+    var now = moment().format("dddd, MMMM Do YYYY")
+    timeDisplay.text(now);
+}
+
+setInterval(displayTime, 1000)
