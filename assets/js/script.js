04dcdb1df40a94061c03ef3d3b6e3852f6d88b12
badtiming <graysonliv@gmail.com>
2022-08-11 20:53:53 -0400

    connected style sheet, added div's for the timeblocks, added save button to save icons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//variable for appending class pastpresentfuture
+//for loop to take the current time and apply past, present or future
+var pastpresentfuture = document.getElementById("#ppf")
+
+// if (condition) {
+    
+// } else if (condition) {
+
+// } else {
+    
+// }
