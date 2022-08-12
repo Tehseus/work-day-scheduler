fc8fdb1c755395f5dc15813080dd809f3ba33d24
badtiming <graysonliv@gmail.com>
2022-08-12 03:03:29 -0400

    cleaned up code that had been commented out while testing functionality, added comments to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-//variable for appending class pastpresentfuture
-//for loop to take the current time and apply past, present or future
-// var pastPresentFuture = document.getElementById("ppf");
+//variable for displaying time under header
 var timeDisplay =  $("#currentDay");
 
-
+//if statement to set current_time to military time for comparison later
 if ("pm" === moment().format("a")&& "12" === moment().format("hh")) {
     var current_time = Number(moment().format("hh"));
 } else if ("am" === moment().format("a")&& "12" === moment().format("hh")) {
@@ -14,16 +12,12 @@ if ("pm" === moment().format("a")&& "12" === moment().format("hh")) {
     var current_time = Number(moment().format("hh"))+12;
 } 
 
-
-// $("#ppf").addClass("present");
-
-
-
+//function that uses variable timeDisplay to format time using moment.js
 function displayTime() {
     var now = moment().format("MMM DD, YYYY [at] hh:mm:ss a")
     timeDisplay.text(now);
 }
-
+//used to call displaytime and it updates every 1000 milliseconds
 setInterval(displayTime, 1000);
 //if now < current_time, set to class future
 //else if now = current_time, set to class present
@@ -31,9 +25,9 @@ setInterval(displayTime, 1000);
 var timeSlot = parseInt($( "div.hour" ).html());
 
 $(document).ready(function(){
-    // Loop through each div element with the class box
+    // Loop through each textarea element with the class form-control
     $("textarea.form-control").each(function(){
-        // Test if the div element is empty
+        // Compares timeSlot and current_time and adds appropriate class to add styling to element
         if (timeSlot < current_time) {
             $(this).addClass("past");
         } else if (timeSlot == current_time) {
@@ -43,12 +37,13 @@ $(document).ready(function(){
         }
     });
 });
-
+//function that should record user input in local storage for recall later, not currently working lol
 $(".saveBtn").on("click", function () {
     var savedInput = $(this).siblings("textarea").val()
     var timeInput = $(this).parent().attr("value")
     localStorage.setItem(timeInput, savedInput);
   });
-for (let i = 0; i < 9; i++) {
+//for loop to recall items saved in local storage, not currently working lol
+for (let i = 0; i < 10; i++) {
     $(`#${i}`).val(localStorage.getItem(i));
 }
